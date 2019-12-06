5412a550a13d5fc2896ed9a3cd9dd7f04e02b98c
Will Boyd <shazzenbot@gmail.com>
2019-12-06 12:04:09 -0500

    add end state

--- src/main/java/com/linuxacademy/azurestoragedd/blob/Main.java
+++ src/main/java/com/linuxacademy/azurestoragedd/blob/Main.java
@@ -1,9 +1,27 @@
 package com.linuxacademy.azurestoragedd.blob;
 
+import com.azure.storage.blob.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Get the Azure Storage service connect string from the CONNECT_STRING environment variable.
+        String connectString = System.getenv("CONNECT_STRING");
+        String containerName = "records";
+        String blobName = "fridge";
+        
+        // BlobServiceClient will interface with the Azure Blob Service.
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectString).buildClient();
+
+        // Get a BlobContainerClient to interface with the container.
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+        
+        // Get a BlobClient to interface with the blob.
+        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        
+        System.out.println("Current Contents of the Breakroom Fridge:");
+        //Download the contents of the file directly to the System.out output stream, causing the blob contents to be printed to the console.
+        blobClient.download(System.out);
     }
 
 }
